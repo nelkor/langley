6866569fc125183a720ac84ad22245dfeec6d67a
nelkor <nelkor@vk.com>
2026-04-27 22:55:46 +0300

    subj

--- packages/words/src/entities/sets/api.ts
+++ packages/words/src/entities/sets/api.ts
@@ -1,9 +1,15 @@
 import { getAsync } from '@/shared'
 import { SupportedLang } from '@/types'
 
-import { calculateSetsForLangs } from './lib'
+import { calculateFinalSet, calculateSetsForLangs } from './lib'
 
 export const getSetsForLangs = (
   nativeLang: SupportedLang,
   targetLang: SupportedLang,
 ) => getAsync(() => calculateSetsForLangs(nativeLang, targetLang))
+
+export const getFinalSet = (
+  nativeLang: SupportedLang,
+  targetLang: SupportedLang,
+  ids: string[],
+) => getAsync(() => calculateFinalSet(nativeLang, targetLang, ids))
--- packages/words/src/entities/sets/lib.ts
+++ packages/words/src/entities/sets/lib.ts
@@ -1,4 +1,4 @@
-import { SupportedLang, SupportedSets } from '@/types'
+import { TrainingWord, SupportedSet, SupportedLang } from '@/types'
 
 import { sets } from './model'
 
@@ -6,12 +6,41 @@ export const calculateSetsForLangs = (
   nativeLang: SupportedLang,
   targetLang: SupportedLang,
 ) => {
-  const result: SupportedSets = []
+  const result: SupportedSet[] = []
 
   sets.forEach(wordSet => {
     if (wordSet.words.some(word => word[nativeLang] && word[targetLang])) {
       result.push({ id: wordSet.id, name: wordSet.name[nativeLang] })
     }
+  })
+
+  return result
+}
+
+export const calculateFinalSet = (
+  nativeLang: SupportedLang,
+  targetLang: SupportedLang,
+  ids: string[],
+) => {
+  const result: TrainingWord[] = []
+
+  sets.forEach(wordSet => {
+    if (!ids.includes(wordSet.id)) {
+      return
+    }
+
+    const words = wordSet.words
+      .map<TrainingWord | null>(word =>
+        word[nativeLang] && word[targetLang]
+          ? {
+              nativeLang: word[nativeLang],
+              targetLang: word[targetLang],
+            }
+          : null,
+      )
+      .filter(Boolean) as TrainingWord[]
+
+    result.concat(words)
   })
 
   return result
