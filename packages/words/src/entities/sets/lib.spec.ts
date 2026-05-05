08b953f4c9d231e3ef53234962e8156d3090bb12
Artem <nelkor@vk.com>
2026-05-05 10:20:25 +0300

    more words (#10)

--- packages/words/src/entities/sets/lib.spec.ts
+++ packages/words/src/entities/sets/lib.spec.ts
@@ -21,6 +21,6 @@ describe('calculateTrainingSet', () => {
   })
 
   it('should calculate correctly', () => {
-    expect(calculateTrainingSet('ru', 'es', ['verbs', 'nouns']).length).toBe(12)
+    expect(calculateTrainingSet('ru', 'es', ['verbs', 'nouns']).length).toBe(40)
   })
 })
