f715997a393a9ed12835a23b26eb3c212bc619fe
Artem <nelkor@vk.com>
2026-05-05 17:39:07 +0300

    training refactoring (#11)

--- packages/web/src/pages/training/types.ts
+++ packages/web/src/pages/training/types.ts
@@ -7,4 +7,14 @@ export type TrainingProps = {
   targetLang: SupportedLang
 }
 
+export type CardProps = {
+  faceUpText: string
+  faceDownText: string
+}
+
+export type ControlsProps = {
+  handleCheck: () => void
+  handleCross: () => void
+}
+
 export type FontSize = 16 | 18 | 20 | 22 | 24 | 26 | 28 | 32 | 36 | 48 | 60 | 72
